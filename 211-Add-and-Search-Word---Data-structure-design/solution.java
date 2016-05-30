a68f5c7ec0e46171c02baeb40335cf280341b9ed
wanqingyou <wanqingyou@users.noreply.github.com>
2016-05-30 15:46:57 -0400

    [211. Add and Search Word - Data structure design][Accepted]committed by LeetCode Extension

--- 211-Add-and-Search-Word---Data-structure-design/solution.java
+++ 211-Add-and-Search-Word---Data-structure-design/solution.java
@@ -1,34 +1,75 @@
 public class WordDictionary {
-    List<String> data=new ArrayList<String>();
+    
+    private Trie trieRoot;
 
-    // Adds a word into the data structure.
-    public void addWord(String word) {
-        if(!data.contains(word)){
-            data.add(word);
-        }
-    }
-
-    // Returns if the word is in the data structure. A word could
-    // contain the dot character '.' to represent any one letter.
-    public boolean search(String word) {
-        if(!isPattern(word)){
-            return data.contains(word);
-        }else{
-            for(String str:data){
-               if(str.matches(word)){
-                   return true;
-               }
-            }
-            return false;
+    class TrieNode {
+        boolean isWord;
+        TrieNode[] children;
+        TrieNode() {
+            this.isWord = false;
+            children = new TrieNode[26];
         }
     }
     
-    private boolean isPattern(String str){
-        if(str.indexOf(".")!=-1){
-            return true;
-        }else{
-            return false;
+    class Trie {
+        TrieNode root;
+        Trie() {
+            root = new TrieNode();
         }
+    
+        public void addWord(String word) {
+           TrieNode node = root, child = null;
+           for (char c: word.toCharArray()) {
+               child = node.children[c-'a'];
+               if (child == null) {
+                   child = new TrieNode();
+                   node.children[c-'a'] = child;
+               }
+               node = child;
+           }
+           node.isWord = true;
+        }
+    
+        public boolean search(String word) {
+            return dfs(root, word);
+        }
+    
+        private boolean dfs(TrieNode root, String word) {
+            if ((!word.isEmpty() && word.charAt(0) != '.' && root.children[word.charAt(0)-'a'] == null) || (word.isEmpty() && !root.isWord)) {
+                return false;
+            }
+            if (word.length() == 0 && root.isWord) {
+                return true;
+            } 
+            boolean ret = false;
+            char c = word.charAt(0);
+            if (c == '.') {
+                for (TrieNode node: root.children) {
+                    if (node != null) {
+                        ret = ret || dfs(node, word.substring(1));
+                    }
+                }
+            } else {
+                ret = dfs(root.children[c-'a'], word.substring(1));
+            }
+            return ret;
+        }
+    
+    }
+    
+    WordDictionary() {
+        trieRoot = new Trie();
+    }
+    
+    // Adds a word into the data structure.
+    public void addWord(String word) {
+        trieRoot.addWord(word);
+    }
+    
+    // Returns if the word is in the data structure. A word could
+    // contain the dot character '.' to represent any one letter.
+    public boolean search(String word) {
+        return trieRoot.search(word);
     }
 }
 
