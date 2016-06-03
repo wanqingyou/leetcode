9dc836dffa84e86f7c348ce9d7ff7ad1f2db4fc2
wanqingyou <wanqingyou@users.noreply.github.com>
2016-06-02 21:33:24 -0400

    [28. Implement strStr()][Accepted]committed by LeetCode Extension

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -1,5 +1,26 @@
 public class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        // return haystack.indexOf(needle);
+        int len=haystack.length();
+        int subLen=needle.length();
+        if(len<subLen) return -1;
+        if(haystack.equals(needle)) return 0;
+        if(needle.equals("")) return 0;
+        int count=0;
+        
+        for(int i=0;i<len-subLen+1;i++){
+            for(int j=0;j<subLen;j++){
+                if(needle.charAt(j)!=haystack.charAt(i+j)){
+                    count=0;
+                    break;
+                }else{
+                    count++;
+                    if(count==subLen){
+                        return i;
+                    }
+                }
+            }
+        }
+        return -1;
     }
 }
