5789f9f48e617ea3a5a0e378283e034c699a71f0
wanqingyou <wanqingyou@users.noreply.github.com>
2016-06-02 21:40:19 -0400

    [6. ZigZag Conversion][Accepted]committed by LeetCode Extension

--- 6-ZigZag-Conversion/solution.java
+++ 6-ZigZag-Conversion/solution.java
@@ -1,42 +1,42 @@
 public class Solution {
-    // public String convert(String s, int numRows) {
-    //     if(s==null) return null;
-    //     if(s.equals("")) return "";
-    //     if(numRows==1) return s;
+    public String convert(String s, int numRows) {
+        if(s==null) return null;
+        if(s.equals("")) return "";
+        if(numRows==1) return s;
         
-    //     StringBuilder sb=new StringBuilder();
-    //     int size = 2*numRows-2;
-    //     for(int i=0;i<numRows;i++){
-    //         for(int j=i;j<s.length();j+=size){
-    //             sb.append(s.charAt(j));
-    //             if(i!=0||i!=numRows-1){
-    //                 int tmp=j+size-2*i;
-    //                 if(tmp<s.length()){
-    //                     sb.append(s.charAt(tmp));
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return sb.toString();
-    // }
-    public String convert(String s, int nRows) {  
-        if(s == null || s.length()==0 || nRows <=0)  
-            return "";  
-        if(nRows == 1)  
-            return s;
-            
-        StringBuilder res = new StringBuilder();  
-        int size = 2*nRows-2;  
-        for(int i=0;i<nRows;i++){  
-            for(int j=i;j<s.length();j+=size){  
-                res.append(s.charAt(j));  
-                if(i != 0 && i != nRows - 1){//except the first row and the last row
-                    int temp = j+size-2*i;
-                    if(temp<s.length())
-                        res.append(s.charAt(temp));
+        StringBuilder sb=new StringBuilder();
+        int size = 2*numRows-2;
+        for(int i=0;i<numRows;i++){
+            for(int j=i;j<s.length();j+=size){
+                sb.append(s.charAt(j));
+                if(i!=0&&i!=numRows-1){
+                    int tmp=j+size-2*i;
+                    if(tmp<s.length()){
+                        sb.append(s.charAt(tmp));
+                    }
                 }
-            }                  
-        }  
-        return res.toString();  
+            }
+        }
+        return sb.toString();
     }
+    // public String convert(String s, int nRows) {  
+    //     if(s == null || s.length()==0 || nRows <=0)  
+    //         return "";  
+    //     if(nRows == 1)  
+    //         return s;
+            
+    //     StringBuilder res = new StringBuilder();  
+    //     int size = 2*nRows-2;  
+    //     for(int i=0;i<nRows;i++){  
+    //         for(int j=i;j<s.length();j+=size){  
+    //             res.append(s.charAt(j));  
+    //             if(i != 0 && i != nRows - 1){//except the first row and the last row
+    //                 int temp = j+size-2*i;
+    //                 if(temp<s.length())
+    //                     res.append(s.charAt(temp));
+    //             }
+    //         }                  
+    //     }  
+    //     return res.toString();  
+    // }
 }
