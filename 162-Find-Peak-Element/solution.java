0bd802b3ae3d323d3bac3175f8f67e0bf2083910
wanqingyou <wanqingyou@users.noreply.github.com>
2016-06-06 18:51:59 -0400

    [162. Find Peak Element][Accepted]committed by LeetCode Extension

--- 162-Find-Peak-Element/solution.java
+++ 162-Find-Peak-Element/solution.java
@@ -1,26 +1,45 @@
 public class Solution {
     public int findPeakElement(int[] nums) {
-        if(nums==null||nums.length==0||nums.length==1) return 0;
-        int result=-1;
-        for(int i=0;i<nums.length;i++){
-            if(i==0){
-                if(nums[i]>nums[i+1]) {
-                    result=i;
-                    break;
-                }
-            }else if(i==nums.length-1){
-                if(nums[i]>nums[i-1]) {
-                    result=i;
-                    break;
-                }
-            }else{
-                if(nums[i]>nums[i-1]&&nums[i]>nums[i+1]){
-                    result=i;
-                    break;
-                }
-            }
+        // if(nums==null||nums.length==0||nums.length==1) return 0;
+        // int result=-1;
+        // for(int i=0;i<nums.length;i++){
+        //     if(i==0){
+        //         if(nums[i]>nums[i+1]) {
+        //             result=i;
+        //             break;
+        //         }
+        //     }else if(i==nums.length-1){
+        //         if(nums[i]>nums[i-1]) {
+        //             result=i;
+        //             break;
+        //         }
+        //     }else{
+        //         if(nums[i]>nums[i-1]&&nums[i]>nums[i+1]){
+        //             result=i;
+        //             break;
+        //         }
+        //     }
             
-        }
-        return result;
+        // }
+        // return result;
+        
+        //O(N)
+        for(int i=1;i<nums.length;i++){  
+            if(nums[i]<nums[i-1])  
+                return i-1;  
+        }  
+        return nums.length-1; 
+        
+        //o(logn)
+        // int left=0,right=nums.length-1;  
+        // while(left<=right){  
+        //     if(left==right)  
+        //         return left;  
+        //     int mid=(left+right)/2;  
+        //     if(nums[mid]<nums[mid+1])  
+        //         left=mid+1;  
+        //     else  
+        //         right=mid;  
+        // }  
     }
 }
