d693cf264241d440724a6ede0bcb7d7c4b470e52
wanqingyou <wanqingyou@users.noreply.github.com>
2016-06-08 21:40:39 -0400

    [92. Reverse Linked List II][Accepted]committed by LeetCode Extension

--- 92-Reverse-Linked-List-II/solution.java
+++ 92-Reverse-Linked-List-II/solution.java
@@ -8,30 +8,56 @@
  */
 public class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        ListNode p1;
-        ListNode p2;
-        int count=1;
-        ListNode cur=head;
-        while(count<m-1){
-            cur=cur.next;
-            count++;
+        // ListNode p1;
+        // ListNode p2;
+        // int count=1;
+        // ListNode cur=head;
+        // while(count<m-1){
+        //     cur=cur.next;
+        //     count++;
+        // }
+        // ListNode firstTail=cur;
+        // cur=cur.next;
+        // ListNode firstHead=cur;
+        // count++;
+        // p1=cur;
+        // p2=cur;
+        // while(count<n){
+        //     p1=cur;
+        //     cur=cur.next;
+        //     p2=cur;
+        //     p2.next=p1;
+        //     count++;
+        // }
+        // firstTail.next=p2;
+        // if(firstHead!=null)
+        //     firstHead.next=p2.next;
+        // return head;
+        
+        if(head == null)
+            return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode preNode = dummy;
+        int i=1;
+        while(preNode.next!=null && i<m)
+        {
+            preNode = preNode.next;
+            i++;
         }
-        ListNode firstTail=cur;
-        cur=cur.next;
-        ListNode firstHead=cur;
-        count++;
-        p1=cur;
-        p2=cur;
-        while(count<n){
-            p1=cur;
-            cur=cur.next;
-            p2=cur;
-            p2.next=p1;
-            count++;
+        if(i<m)
+            return head;
+        ListNode mNode = preNode.next;
+        ListNode cur = mNode.next;
+        while(cur!=null && i<n)
+        {
+            ListNode next = cur.next;
+            cur.next = preNode.next;
+            preNode.next = cur;
+            mNode.next = next;
+            cur = next;
+            i++;
         }
-        firstTail.next=p2;
-        if(firstHead!=null)
-            firstHead.next=p2.next;
-        return head;
+        return dummy.next;
     }
 }
