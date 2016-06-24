0f5879111cb0df45a74956973086f7ac775caa60
wanqingyou <wanqingyou@users.noreply.github.com>
2016-06-24 17:01:12 -0400

    [147. Insertion Sort List][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 147-Insertion-Sort-List/solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if(head==null||head.next==null) return head;
+        ListNode cur=head;
+        ListNode rever=head;
+        ListNode pre=null;
+        ListNode p=null;
+        
+        while(cur!=null){
+            pre=cur;
+            cur=cur.next;
+            rever=cur;
+            rever.next=pre;
+            p=rever;
+            int key=p.val;
+            while(p.next!=null){
+                if(key<p.next.val){
+                    p.val=p.next.val;
+                }
+                p=p.next;
+            }
+            p.val=key;
+        }
+        
+        return head;
+    }
+    
+}
