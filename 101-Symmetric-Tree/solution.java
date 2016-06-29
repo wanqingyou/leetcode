dcf625085667b3bb4db0ceb67856aaf8f89db6a0
wanqingyou <wanqingyou@users.noreply.github.com>
2016-06-29 17:26:10 -0400

    [101. Symmetric Tree][Accepted]committed by LeetCode Extension

--- 101-Symmetric-Tree/solution.java
+++ 101-Symmetric-Tree/solution.java
@@ -8,7 +8,7 @@
  * }
  */
 public class Solution {
-    public boolean isSymmetric(TreeNode root) {
+    public boolean isSymmetric1(TreeNode root) {
         if(root==null) return true;
         return isSym(root.left,root.right);
     }
@@ -25,5 +25,28 @@ public class Solution {
         if(!isSym(left.right, right.left))  
             return false;  
       return true;  
+    }
+    
+    //interative
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        LinkedList<TreeNode> lq=new LinkedList<TreeNode>();
+        LinkedList<TreeNode> rq=new LinkedList<TreeNode>();
+        lq.add(root.left);
+        rq.add(root.right);
+        while(!lq.isEmpty()&&!rq.isEmpty()){
+            TreeNode left=lq.pollFirst();
+            TreeNode right=rq.pollFirst();
+            if(left==null&&right==null) continue;
+            if(left==null&&right!=null) return false;
+            if(left!=null&&right==null) return false;
+            if(left.val != right.val)  
+                return false;
+            lq.add(left.left);
+            lq.add(left.right);
+            rq.add(right.right);
+            rq.add(right.left);
+        }
+        return true;
     }
 }
