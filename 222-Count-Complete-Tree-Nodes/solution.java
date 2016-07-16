a17b4b841fc95e488e6de15d32f62ac35016613e
wanqingyou <wanqingyou@users.noreply.github.com>
2016-07-16 16:51:27 -0400

    [222. Count Complete Tree Nodes][Time Limit Exceeded]committed by LeetCode Extension

--- 222-Count-Complete-Tree-Nodes/solution.java
+++ 222-Count-Complete-Tree-Nodes/solution.java
@@ -10,12 +10,36 @@
 public class Solution {
     public int countNodes(TreeNode root) {
         if(root==null) return 0;
-        return numOfNode(root);
+        int l=getLeft(root)+1;
+        int r=getRight(root)+1;
+        
+        if(l==r) {  
+            return (2<<(l-1)) - 1;  
+        } else {  
+            return numOfNode(root);
+        }
         
     }
     private int numOfNode(TreeNode root){
         if(root==null) return 0;
         if(root.left==null&&root.right==null) return 1;
         return numOfNode(root.left)+numOfNode(root.right)+1;
+    }
+    private int getLeft(TreeNode root){
+        int count=0;
+        while(root.left!=null){
+            count++;
+            root=root.left;
+        }
+        return count;
+    }
+    
+    private int getRight(TreeNode root){
+        int count=0;
+        while(root.right!=null){
+            count++;
+            root=root.right;
+        }
+        return count;
     }
 }
