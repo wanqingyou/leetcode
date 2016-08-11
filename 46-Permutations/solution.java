9de2fc0519441837357f0c0988eb149c05464111
wanqingyou <wanqingyou@users.noreply.github.com>
2016-08-11 17:50:12 -0400

    [46. Permutations][Accepted]committed by LeetCode Extension

---
+++ 46-Permutations/solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length == 0) return result;
+
+        List<Integer> list = new ArrayList<Integer>();
+        dfs(nums, list, result);
+
+        return result;
+    }
+
+    private void dfs(int[] nums, List<Integer> list, List<List<Integer>> result) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<Integer>(list));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i])) continue;
+            list.add(nums[i]);
+            dfs(nums, list, result);
+            list.remove(list.size() - 1);
+        }
+    }
+}
